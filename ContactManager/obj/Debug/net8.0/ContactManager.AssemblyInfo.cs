c3c84ba4f1c46a739b147f9ab07f510cea080099
pkeenaphan91@gmail.com <pkeenaphan91@gmail.com>
2024-04-07 01:42:44 -0400

    Final commit

--- ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
+++ ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfba979cc9498b0148aedc7bda93aeba34d97a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
